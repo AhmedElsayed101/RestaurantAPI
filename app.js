c655e160dabd9f6064b0998d7ec2a18e8b93e2f3
AhmedElsayed101 <elsayedahmed945@gmail.com>
2020-10-17 14:40:45 +0200

    created basic authentication

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const dishRouter = require('./routes/dishRouter')
 const leaderRouter = require('./routes/leadersRouter')
 const promotionRouter = require('./routes/promotionsRouter')
 
-const Dishes = require('./models/dishes')
+const Dishes = require('./models/dishes');
+const { Buffer } = require('buffer');
 const dbUrl = 'mongodb://localhost:27017/conFusion'
 const connect = mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true ,useCreateIndex : true,})
 
@@ -31,6 +32,38 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log('headers', req.headers) 
+
+  let authHeader = req.headers.authorization
+
+  if (!authHeader) {
+
+    let err = new Error ("You are not authenticated!!")
+    res.setHeader('WWW-Authenticate', 'Basic')
+    err.status = 401
+    return next(err)
+  }
+  let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
+  const username = auth[0]
+  const password = auth[1]
+  console.log('user', username)
+  console.log('pass', password)
+  if(username === 'admin' && password === 'password'){
+    next()
+  }
+  else{
+    let err = new Error ("You are not authenticated!!")
+    res.setHeader('WWW-Authenticate', 'Basic')
+    err.status = 401
+    return next(err)
+  }
+}
+
+
+app.use(auth)
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
