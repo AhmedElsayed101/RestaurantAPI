3e5f9e32d251849ca48701ad843404440c4f39de
Ahmed Elsayed <elsayedahmed945@gmail.com>
2020-11-20 20:38:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey' : '12345',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook' : {
-        clientId: '350813992817584',
-        clientSecret : '1f91ea4be31b0258807cb4d7e28a52f4'
+        clientId: '3508139928',
+        clientSecret : '1f91ea458807cb4'
     }
 }
